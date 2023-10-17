b4660e0c51f7b611587e582c039c6e8f148891ba
L1py <tomaslipavsky965@gmail.com>
2023-10-17 10:57:45 +0200

    gg

---
+++ res/js/main.js
@@ -0,0 +1,11 @@
+
+// document.getElementById() - ziskaelement z html dokumentu
+document.getElementById("cookie");
+document.getElementById("counter");
+
+const PI = 3.14; /*const - promenna co se nemeni, let - promenna co se muze menit*/
+let number = 0;
+console.log(number);
+ number = 10;
+console.log(number);
+console.log(PI);
